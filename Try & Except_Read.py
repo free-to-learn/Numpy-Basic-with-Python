28fe2dcb344fac909f2808d914848839051f72e3
REDDY PRASADE <15084222+reddyprasade@users.noreply.github.com>
2019-07-09 17:06:48 +0530

    Delete Try & Except_Read.py

--- Try & Except_Read.py
+++
@@ -1,9 +0,0 @@
-try:
-    f = open('testfile','r')
-    f.write('Test write this')
-except IOError:
-    # This will only check for an IOError exception and then execute this print statement
-   print ("Error: Could not find file or read data")
-else:
-   print ("Content written successfully")
-   f.close()
